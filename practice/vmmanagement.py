bb1979f90c26e2d3df5a8e031da29e42f0b08119
habahram <hamid.bahramian@gmail.com>
2019-03-08 00:04:21 -0500

    vm management sample

--- practice/vmmanagement.py
+++ practice/vmmanagement.py
@@ -8,6 +8,7 @@ config = Config()
 EC2_ACCESS_ID = config['cloudmesh.cloud.aws.credentials.EC2_ACCESS_ID']
 EC2_SECRET_KEY = config['cloudmesh.cloud.aws.credentials.EC2_SECRET_KEY']
 
+
 EC2Driver = get_driver(Provider.EC2)
 conn = EC2Driver(EC2_ACCESS_ID, EC2_SECRET_KEY)
 
