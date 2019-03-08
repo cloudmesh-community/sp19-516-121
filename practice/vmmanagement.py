e665a5bc071e4ca2c71487a998edff37b92574de
habahram <hamid.bahramian@gmail.com>
2019-03-08 00:00:25 -0500

    vm management sample

---
+++ practice/vmmanagement.py
@@ -0,0 +1,14 @@
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
+from cloudmesh.management.configuration.config import Config
+
+config = Config()
+
+EC2_ACCESS_ID = config['cloudmesh.cloud.aws.credentials.EC2_ACCESS_ID']
+EC2_SECRET_KEY = config['cloudmesh.cloud.aws.credentials.EC2_SECRET_KEY']
+
+EC2Driver = get_driver(Provider.EC2)
+conn = EC2Driver(EC2_ACCESS_ID, EC2_SECRET_KEY)
+
+
