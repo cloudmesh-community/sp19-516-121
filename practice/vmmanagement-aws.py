1847b0a2d431099c74cbeec93c57ad1eaf52e0e3
habahram <hamid.bahramian@gmail.com>
2019-03-10 00:47:14 -0500

    vm management sample

--- practice/vmmanagement-aws.py
+++ practice/vmmanagement-aws.py
@@ -3,6 +3,7 @@ from libcloud.compute.providers import get_driver
 
 
 import pprint
+import time
 
 from cloudmesh.management.configuration.config import Config
 
@@ -26,17 +27,22 @@ nodes = conn.list_nodes()
 for node in nodes:
     pprint.pprint(node)
 
+nodename = "iu-cloudcomputing-test2"
+
 vmmanager = VMManager(conn)
 
-if len(nodes) == 0:
-    node = vmmanager.create_node("iu-cloudcomputing-test1", IMAGE_ID, SIZE_ID,ACCESS_KEY_NAME, SECURITY_GROUP_NAMES)
+selectednodes = [n for n in nodes if n.name == nodename]
+
+if len(selectednodes) == 0:
+    node = vmmanager.create_node(nodename, IMAGE_ID, SIZE_ID,ACCESS_KEY_NAME, SECURITY_GROUP_NAMES)
 else:
-    node = nodes[0]
+    node = selectednodes[0]
+
 
 if(node.state.lower() != "running"):
-    conn.ex_start(node=node)
+    conn.ex_start_node(node)
 
-#conn.ex_stop(node=node)
+time.sleep(60)
 
 conn.destroy_node(node)
 
