ba91a19f0b03333e776e451db82c97a1ebaf864e
habahram <hamid.bahramian@gmail.com>
2019-03-21 15:09:59 -0700

    project

---
+++ project-code/test.py
@@ -0,0 +1,21 @@
+import js2py
+
+import oyaml as yaml
+
+
+with open("workflow.yaml", "r") as stream:
+    data = yaml.load(stream)
+
+tasks = data["tasks"]
+for task in tasks:
+    print(task)
+
+def visualize(workflow):
+    flows = workflow.split("|")
+
+
+
+
+
+
+
